2e347faf9957d94bff9ba7a0561b0d4e42117bc1
Akhil S Shekar <akhilshekar0@gmail.com>
2026-02-21 06:19:43 +0530

    updated

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const bookmarkRoutes = require('./routes/bookmarkRoutes');
+// DELETE OR COMMENT OUT THE LINE BELOW
+// const { clerkMiddleware } = require('@clerk/express'); 
 require('dotenv').config();
 
 const app = express();
 connectDB();
 
-// CORS configuration for Vercel deployment
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// DELETE OR COMMENT OUT THE LINE BELOW
+// app.use(clerkMiddleware()); 
+
 app.use('/api/bookmarks', bookmarkRoutes);
 
 const PORT = process.env.PORT || 5000;
