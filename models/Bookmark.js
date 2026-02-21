2fe45df897845caa62bc25af12b191ff0ee8901e
Akhil S Shekar <akhilshekar0@gmail.com>
2026-02-21 06:09:09 +0530

    updates

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const bookmarkSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    url: { type: String, required: true },
-    userId: { type: String, required: true } 
+    url: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Bookmark', bookmarkSchema);
